a238c9e9e49d87d5b71e9ff3adac98cfa410e2f4
cardoX007 <cardoelcapone@gmail.com>
2021-02-13 00:07:57 -0800

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">Header</div>
+      <div className="menu">Menu</div>
+      <div className="content">Content</div>
+      <div className="footer">Footer</div>
+      
+    </div>
+  );
+}
+
+export default App;
